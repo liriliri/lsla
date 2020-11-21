25dee8eff0cb8458556eb0a7fc98c7e512e54ee2
surunzi <redhoodsu@tencent.com>
2020-11-21 20:29:53 +0800

    feat: shx

--- bin/lsla.js
+++ bin/lsla.js
@@ -3,6 +3,7 @@
 const parseArgs = require('licia/parseArgs')
 const contain = require('licia/contain')
 const execa = require('execa')
+const path = require('path')
 
 const options = parseArgs(process.argv.slice(2), {
   names: {
@@ -16,11 +17,14 @@ const remain = options.remain
 
 const cmd = remain[0]
 
-if (contain(['prettier'], cmd)) {
-  execa(cmd, process.argv.slice(3), {
-    preferLocal: true,
-    stdio: 'inherit',
-  })
+if (contain(['prettier', 'shx'], cmd)) {
+  execa(
+    path.resolve(__dirname, '../node_modules/.bin', cmd),
+    process.argv.slice(3),
+    {
+      stdio: 'inherit',
+    }
+  )
 } else if (contain(['genIcon'], cmd)) {
   require('../' + cmd)(options)
 } else {
