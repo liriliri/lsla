ac2767ae25e8b1c8d108653d57851a82d324d727
surunzi <redhoodsu@tencent.com>
2020-11-21 19:41:09 +0800

    feat: prettier

--- bin/lsla.js
+++ bin/lsla.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const parseArgs = require('licia/parseArgs')
+const contain = require('licia/contain')
+const execa = require('execa')
 
 const options = parseArgs(process.argv.slice(2), {
   names: {
@@ -14,4 +16,13 @@ const remain = options.remain
 
 const cmd = remain[0]
 
-require('../' + cmd)(options)
+if (contain(['prettier'], cmd)) {
+  execa(cmd, process.argv.slice(3), {
+    preferLocal: true,
+    stdio: 'inherit',
+  })
+} else if (contain(['genIcon'], cmd)) {
+  require('../' + cmd)(options)
+} else {
+  console.log('Cmd not supported')
+}
