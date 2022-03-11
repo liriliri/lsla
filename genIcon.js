23f54bc9bd406e1655a5bdb82eeb26b1b7437079
redhoodsu <redhoodsu@tencent.com>
2022-03-11 20:00:56 +0800

    feat: read icon.json

--- genIcon.js
+++ genIcon.js
@@ -4,6 +4,7 @@ const map = require('licia/map')
 const filter = require('licia/filter')
 const endWith = require('licia/endWith')
 const promisify = require('licia/promisify')
+const each = require('licia/each')
 const path = require('path')
 
 const generate = promisify(webfontsGenerator)
@@ -14,9 +15,17 @@ module.exports = async function (options) {
   const iconDir = path.resolve(input)
   let files = await fs.readdir(iconDir)
   files = filter(files, (file) => endWith(file, '.svg'))
+  files = map(files, (file) => path.resolve(iconDir, file))
+  const indexPath = iconDir + '/icon.json'
+  if (await fs.exists(indexPath)) {
+    const icons = JSON.parse(await fs.readFile(indexPath, 'utf8'))
+    each(icons, icon => {
+      files.push(path.resolve(iconDir, icon))
+    })
+  }
   const dest = path.resolve(__dirname, './icon')
   const result = await generate({
-    files: map(files, (file) => iconDir + '/' + file),
+    files,
     types: ['woff'],
     cssTemplate: path.resolve(__dirname, 'cssTemplate.hbs'),
     dest,
